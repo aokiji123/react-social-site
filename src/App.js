2dc8f85da618cb30aa6bd632f4f0dd3a76a1b92d
aokiji123 <jenkaboy7@gmail.com>
2022-04-10 00:49:54 +0300

    profile and messages routing

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const App = () => {
 
 				<div className={styles.grid}>
 					<Routes>
-						<Route path="/profile" element={<Profile/>}/>
+						<Route path="" element={<Profile/>}/>
 						<Route path="/messages" element={<Messages/>}/>
 					</Routes>
 				</div>
