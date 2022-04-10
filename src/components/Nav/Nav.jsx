6aa131558c01c0e22440f29d02b7d0c01d59e576
aokiji123 <jenkaboy7@gmail.com>
2022-04-10 16:44:58 +0300

    'messages'

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom"
 import style from "./Nav.module.css"
 
 const Nav = () => {
@@ -6,8 +7,8 @@ const Nav = () => {
 
         <nav className="nav">
             <ul className={style.group}>
-                <li><a className={`${style.item} ${style.active}`} href="/profile">Profile</a></li>
-                <li><a className={style.item} href="/messages">Messages</a></li>
+                <li><NavLink className={navData => navData.isActive ? style.active : style.item} to="/profile">Profile</NavLink></li>
+                <li><NavLink className={navData => navData.isActive ? style.active : style.item} to="/messages">Messages</NavLink></li>
                 <li><a className={style.item} href="s#">News</a></li>
                 <li><a className={style.item} href="s#">Music</a></li>
             </ul>
--- src/components/Profile/Messages/Messages.jsx
+++ src/components/Profile/Messages/Messages.jsx
@@ -1,41 +1,40 @@
-// import Header from "../../Header/Header"
-// import Nav from "../../Nav/Nav"
 import style from "./Messages.module.css"
-
 
 const Messages = () => {
     return (
-        <div>
-            {/* <Header/>
-            <Nav/> */}
-            
+        <div>            
             <div className={style.block}>
 
                 <div className={style.block__people}>
-                        <h3 className={style.name}>Masha</h3>
-                        <h3 className={style.name}>Dima</h3>
-                        <h3 className={style.name}>Nastya</h3>
-                        <h3 className={style.name}>Tanya</h3>
-                        <h3 className={style.name}>Bogdan</h3>
-                        <h3 className={style.name}>Masha</h3>
-                        <h3 className={style.name}>Masha</h3>
+                    <h3 className={style.name}>Den</h3>
+                    <h3 className={`${style.name} ${style.active}`}>Mike</h3>
+                    <h3 className={style.name}>Rebbeka</h3>
+                    <h3 className={style.name}>Jesse</h3>
+                    <h3 className={style.name}>Clam</h3>
+                    <h3 className={style.name}>Kate</h3>
+                    <h3 className={style.name}>Vanessa</h3>
                 </div>
 
                 <div className={style.block__text}>
-                    <img src="" alt="Avatar"/>
-                    <div className={style.message}>
+                    <img className={style.friend} src="images/avatar1.png" alt="Avatar"/>
+                        <div className={style.message}>
                         <div className={style.text}>hi how are you mate?</div>
-                        <div className={style.date}>22:11</div>
                     </div>
-                    <img src="" alt="Avatar"/>
+                
+                    <img className={style.me} src="images/avatar.png" alt="Avatar"/>
                     <div className={style.message}>
                         <div className={style.text}>hello</div>
-                        <div className={style.date}>22:11</div>
+                        <div className={style.date}></div>
                     </div>
-                    <img src="" alt="Avatar"/>
+
                     <div className={style.message}>
                         <div className={style.text}>well i'm kinda tired, try to take some rest watching youtube</div>
-                        <div className={style.date}>22:11</div>
+                        <div className={style.date}></div>
+                    </div>
+
+                    <div className={style.message}>
+                        <div className={style.text}>today was studying a lot</div>
+                        <div className={style.date}></div>
                     </div>
                     
                 </div>
