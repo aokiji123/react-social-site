922420f1451a67bb5a6273174b7e4b37bd3863b4
aokiji123 <jenkaboy7@gmail.com>
2022-04-21 01:16:42 +0300

    post adding, flux concept

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,20 +1,32 @@
+import React from "react"
 import Posts from "./Posts/Posts"
+import ProfilePerson from "./ProfilePerson"
 import style from "./Profile.module.css"
 
 const Profile = (props) => { 
     
+    let newPost = React.createRef()
+
+    let addPost = () => {
+        props.addPost()
+    }
+
+    let onPostChange = () => {
+        let text = newPost.current.value
+        props.updateNewPostData(text)
+    }
+
     return (
         <div className={style.content}>
-            <div>
-                <img className={style.image} src="./images/header-img.jpeg" alt="Main site image"/>
-            </div>
-            <div className={style.info}>
-                <img className={style.avatar} src="./images/avatar.png" alt="Avatar"/>
-                <p className={style.text}>
-                    Bogdan Tolmachev <br/>
-                    <span className={style.job}>React Developer</span>
-                </p>
-            </div>
+            
+            <ProfilePerson/>
+
+            <section>
+                <form action="" type="submit">
+                    <input className="input" type="text" placeholder="Add new post" ref={newPost} onChange={onPostChange} value={props.state.newPostData} />
+                    <button onClick={addPost} className="button" type="button"> &#10003; </button>
+                </form>
+            </section>
 
             <Posts postData={props.state.postData}/>
 
