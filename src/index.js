3d5aaea00fa68b3827235662852b2300a9a27eb9
aokiji123 <jenkaboy7@gmail.com>
2022-04-26 00:42:57 +0300

    dispatches

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { rerenderEntireTree } from "./render.js"
-import state from "./redux/state"
 
-rerenderEntireTree(state)
+import store from "./redux/state"
+
+import { BrowserRouter } from 'react-router-dom';
+
+export let rerenderEntireTree = () => {
+  ReactDOM.render( 
+    <React.StrictMode>
+      <BrowserRouter>
+        <App
+          state={store.getState()}
+          dispatch={store.dispatch.bind(store)}
+        />
+      </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
+  )
+}
+
+rerenderEntireTree(store.getState())
 reportWebVitals()
+
+store.subscribe(rerenderEntireTree)
